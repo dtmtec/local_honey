118a3d44ae4df9a672fc71ef29525f2a7c0cdd73
Luis Vasconcellos <luisvasconcellos@Luiss-MacBook-Pro.local>
2013-10-01 19:25:28 -0300

    Refactoring to use two different layouts

---
+++ app/helpers/local_honey/local_honey_helper.rb
@@ -0,0 +1,12 @@
+module LocalHoney
+  module LocalHoneyHelper
+    def current_locale
+      image_tag("local_honey/flag-#{I18n.locale}.png")
+    end
+
+    def render_local_honey(options = {})
+      options = options.reverse_merge({ layout: :select })
+      render "locales/#{options[:layout]}_layout"
+    end
+  end
+end
--- app/helpers/local_honey/locale_helper.rb
+++
@@ -1,7 +0,0 @@
-module LocalHoney
-  module LocaleHelper
-    def current_locale
-      image_tag("local_honey/flag-#{I18n.locale}.png")
-    end
-  end
-end
