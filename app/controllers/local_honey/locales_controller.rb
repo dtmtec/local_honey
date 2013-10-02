b10a7b21c15c98e9f01b91e1510d77c427bdb36f
Luis Vasconcellos <luisvasconcellos@Luiss-MacBook-Pro.local>
2013-10-02 12:17:26 -0300

    Adding the current_locale to the session

--- app/controllers/local_honey/locales_controller.rb
+++ app/controllers/local_honey/locales_controller.rb
@@ -2,6 +2,7 @@ module LocalHoney
   class LocalesController < LocalHoney::ApplicationController
     def locale
       I18n.locale = params[:locale]
+      session[:current_locale] = params[:locale]
       redirect_to request.referrer
     end
   end
